52841d536a4c127cb4e3e844624453975eb90c9e
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-04-20 15:36:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,8 +18,12 @@ def assign_rooms(speakers)
   roomlist
 end
 
-def printer(attendees)
-  batch_badge_creator(attendees).each do |id|
-  puts id
+def printer(array)
+  batch_badge_creator(array).each do |id|
+    puts id
+  end 
+  
+  assign_rooms(array).each do |id|
+    puts id 
   end
-end
+end 
