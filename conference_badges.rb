509c9b9b5ffdb368d10f508a57d99529e5381d44
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-04-20 15:35:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(namearray)
+  batches = []
+  namearray.each do |name|
+    batches.push(badge_maker(name))
+  end
+  batches
+end
+
+def assign_rooms(speakers)
+  roomlist = []
+  speakers.each_with_index do |speaker, index|
+    roomlist.push("Hello, #{speaker}! You'll be assigned to room #{index+1}!")
+  end
+  roomlist
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |id|
+  end
+end
